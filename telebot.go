f58a5661942cb3eee68c1426e1e15d5eb777f25a
Emeka Icha <emeka.icha@gmail.com>
2017-09-20 17:01:55 +0300

    add send message

--- telebot.go
+++ telebot.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"bytes"
 	"net/http"
+	"fmt"
+	"encoding/json"
 )
 
 type Bot struct {
@@ -29,7 +31,7 @@ func (*b Bot) getMe() (User, error) {
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/%s", b.Token, "getMe")
 
 	var buf bytes.Buffer
-	
+
 	resp, err := http.Post(url, "application/json", &buf)
 	if err != nil {
 		return []byte{}, errors.Wrap(err, "http.Post failed")
@@ -42,4 +44,24 @@ func (*b Bot) getMe() (User, error) {
 	}
 
 return json, nil
+}
+
+func (*b Bot) sendMessage(chatID int64, text string) (*m Message, error) {
+
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=%s", b.Token, chatID, text)
+
+	var buf bytes.Buffer
+	
+		resp, err := http.Post(url, "application/json", &buf)
+
+		if err != nil {
+			return Message{}, err 
+		}
+
+		var message Message
+
+		json.Unmarshal(resp.Body, &message)
+
+		return message, nil 
+
 }
