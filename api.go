2859c386b0560fa27783ae35ba8c14b0dc2dab14
Emeka Icha <emeka.icha@gmail.com>
2017-09-27 12:39:04 +0300

    refactor http requests into a function

--- api.go
+++ api.go
@@ -13,23 +13,17 @@ func (b *Bot) getMe() (User, error) {
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/%s", b.Token, "getMe")
 
 
-	resp, err := http.Get(url)
+	resp, err := getContent(url)
 
-	if err != nil {
-		return User{}, err 
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return User{}, err
 	}
 
 	var user User 
-	err = json.Unmarshal(body, &user)
+	err = json.Unmarshal(resp, &user)
+
 	if err != nil {
-		return User{}, nil 
+		return User{}, err
 	}
 	return user, nil
 
@@ -117,4 +111,26 @@ func (b *Bot) getUpdates(offset int64, timeout int64)([]Update,error) {
 	}
 
 	return updatesReceived.Result, nil 
+}
+
+func getContent(url string)([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close() 
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err 
+	}
+
+	return body, nil 
 }
