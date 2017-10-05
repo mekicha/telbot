03b68deca511ccbb73e0a5662b5cc11cfb6ea4eb
mekicha <emeka.icha@gmail.com>
2017-10-05 21:14:03 +0300

    format methods

--- api.go
+++ api.go
@@ -1,13 +1,12 @@
 package telebot
 
 import (
-	"io/ioutil"
-	"net/http"
-	"fmt"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
-
 
 func (b *Bot) getMe() (User, error) {
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/getMe", b.Token)
@@ -19,70 +18,71 @@ func (b *Bot) getMe() (User, error) {
 	}
 
 	var botInfo struct {
-		Ok bool 
-		Result User 
-		Description string 
+		Ok          bool
+		Result      User
+		Description string
 	}
-	
+
 	err = json.Unmarshal(resp, &botInfo)
 	if err != nil {
 		return User{}, err
 	}
-	
+
 	if !botInfo.Ok {
 		return User{}, errors.New("bad response")
 	}
 
-	return botInfo.Result, nil 
+	return botInfo.Result, nil
 
 }
 
+// SendMessage sends message to a user
 func (b *Bot) SendMessage(chatID int64, text string) (Message, error) {
 
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=%s", b.Token, chatID, text)
 
 	resp, err := getContent(url)
-	
-		if err != nil {
-			return Message{}, err 
-		}
-	
-		var m Message
-		err = json.Unmarshal(resp, &m)
 
-		if err != nil {
-			return Message{}, err
-		}
+	if err != nil {
+		return Message{}, err
+	}
 
-		return m, nil
+	var m Message
+	err = json.Unmarshal(resp, &m)
+
+	if err != nil {
+		return Message{}, err
+	}
+
+	return m, nil
 
 }
 
-func (b *Bot) SendToChannel(channelName string, text string)(error) {
+// SendToChannel sends message to a registered channel @channelName
+func (b *Bot) SendToChannel(channelName string, text string) error {
 
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s", b.Token, channelName, text)
 
 	_, err := getContent(url)
 
 	if err != nil {
-		return err 
+		return err
 	}
 
-	return nil 
+	return nil
 }
 
-
-
-func (b *Bot) GetUpdates(offset int64, timeout int64)([]Update,error) {
+// GetUpdates gets update from telegram via long polling
+func (b *Bot) GetUpdates(offset int64, timeout int64) ([]Update, error) {
 
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/getUpdates?offset=%d&timeout=%d", b.Token, offset, timeout)
-	
+
 	resp, err := http.Get(url)
 
 	var updatesReceived struct {
-		Ok bool 
-		Result []Update 
-		Description string 
+		Ok          bool
+		Result      []Update
+		Description string
 	}
 
 	decoder := json.NewDecoder(resp.Body)
@@ -97,10 +97,10 @@ func (b *Bot) GetUpdates(offset int64, timeout int64)([]Update,error) {
 		return updatesReceived.Result, errors.New(updatesReceived.Description)
 	}
 
-	return updatesReceived.Result, nil 
+	return updatesReceived.Result, nil
 }
 
-func getContent(url string)([]byte, error) {
+func getContent(url string) ([]byte, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -112,16 +112,17 @@ func getContent(url string)([]byte, error) {
 		return nil, err
 	}
 
-	defer resp.Body.Close() 
+	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err 
+		return nil, err
 	}
 
-	return body, nil 
+	return body, nil
 }
 
+// SetWebhook sets a url as webhook. This disables GetUpdates
 func (b *Bot) SetWebhook(url string) bool {
 	endpoint := fmt.Sprintf("https://api.telegram.org/bot%s/setWebhook?url=%s", b.Token, url)
 	resp, err := http.Get(endpoint)
@@ -135,21 +136,23 @@ func (b *Bot) SetWebhook(url string) bool {
 	err = decoder.Decode(&res)
 
 	if !res.Ok {
-		return false 
+		return false
 	}
-	return true 
+	return true
 }
 
+// DeleteWebhook removes webhook url
 func (b *Bot) DeleteWebhook() bool {
 	url := fmt.Sprintf("https://api.telegram.com/bot%s/deleteWebhook", b.Token)
 	_, err := http.Get(url)
 	if err != nil {
-		return false 
+		return false
 	}
-	return true 
+	return true
 }
 
-func (bot *Bot) ListenForWebhook(pattern string) UpdatesChannel {
+// ListenForWebhook gets updates sent to the webhook url
+func (b *Bot) ListenForWebhook(pattern string) UpdatesChannel {
 	ch := make(chan Update, 10)
 
 	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
